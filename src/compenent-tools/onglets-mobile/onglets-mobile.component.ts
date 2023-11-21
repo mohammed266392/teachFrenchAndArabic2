72ffc6cec540c698dc9789696839ffc2092f78b4
raki <mohammed.raki26@gmail.com>
2023-11-21 09:14:55 +0100

    section onglet for mobile

---
+++ src/compenent-tools/onglets-mobile/onglets-mobile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-onglets-mobile',
+  templateUrl: './onglets-mobile.component.html',
+  styleUrls: ['./onglets-mobile.component.css']
+})
+export class OngletsMobileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
