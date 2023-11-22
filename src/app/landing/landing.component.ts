2eb1c77fff7408d3bd6f1cb3c20bb8eb6e8a636e
raki <mohammed.raki26@gmail.com>
2023-11-22 15:41:57 +0100

    section onglet for mobile : partie 2

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
+  nbImageEchantillon = [
+    {id:1,srcLink:"./../../assets/images/bulle-marbre1.png"},
+    {id:2,srcLink:"./../../assets/images/bulle-marbre2.png"},
+    {id:3,srcLink:"./../../assets/images/bulle-marbre3.png"},
+    {id:4,srcLink:"./../../assets/images/bulle-marbre4.png"},
+    {id:4,srcLink:"./../../assets/images/bulle-marbre5.png"},
+    {id:4,srcLink:"./../../assets/images/bulle-marbre6.png"}
+  ]
 
   constructor() { }
 
