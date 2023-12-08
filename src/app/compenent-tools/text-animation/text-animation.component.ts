ed671865b6fe5b7cbd1d659692b60af49d2e4870
raki <mohammed.raki26@gmail.com>
2023-12-08 12:34:58 +0100

    texte animation

---
+++ src/app/compenent-tools/text-animation/text-animation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-animation',
+  templateUrl: './text-animation.component.html',
+  styleUrls: ['./text-animation.component.css'],
+  standalone:true
+})
+export class TextAnimationComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { Landing2Component } from '../landing2/landing2.component';
 import { SavoirFaire2Component } from '../savoir-faire2/savoir-faire2.component';
+import { TextAnimationComponent } from '../compenent-tools/text-animation/text-animation.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone:true,
-  imports :  [Landing2Component, SavoirFaire2Component]
+  imports :  [TextAnimationComponent, Landing2Component, SavoirFaire2Component]
 })
 export class HomeComponent {
 
